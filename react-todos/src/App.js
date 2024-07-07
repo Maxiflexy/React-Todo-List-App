12be33cf01c3640b9ed3d5c5befa6170f2aeef54
Maxiflexy <onyemax247@gmail.com>
2024-07-07 01:12:22 +0100

    Initial commit

--- react-todos/src/App.js
+++ react-todos/src/App.js
@@ -1,30 +1,19 @@
 import "./App.css";
-import TodoRowItem from "./components/TodoRowItems";
+import TodoTable from "./components/TodoTable";
 
 function App() {
+  const todos = [
+    { rowNumber: 1, rowDescription: "Feed puppy", rowAssigned: "User One" },
+    { rowNumber: 2, rowDescription: "Water plants", rowAssigned: "User Two" },
+    { rowNumber: 3, rowDescription: "Make dinner", rowAssigned: "User One" },
+  ];
+
   return (
     <div className="mt-5 container">
       <div className="card">
         <div className="card-header">Your Todo's</div>
         <div className="card-body">
-          <table className="table table-hover">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Description</th>
-                <th scope="col">Assigned</th>
-              </tr>
-            </thead>
-
-            <tbody>
-              <TodoRowItem />
-              <tr>
-                <th scope="row">2</th>
-                <td>Get haircut</td>
-                <td>Eric</td>
-              </tr>
-            </tbody>
-          </table>
+          <TodoTable todos={todos}/>
         </div>
       </div>
     </div>
