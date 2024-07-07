8a1e51e059e476fcb44c366b35585c3e5a709438
Maxiflexy <onyemax247@gmail.com>
2024-07-07 09:48:44 +0100

    newTodoForm commit

--- react-todos/src/components/TodoTable.js
+++ react-todos/src/components/TodoTable.js
@@ -1,34 +1,26 @@
-import TodoRowItem from "./TodoRowItems"
+import TodoRowItem from "./TodoRowItems";
 
 function TodoTable(props) {
-    return (
-        <table className="table table-hover">
-            <thead>
-                <tr>
-                    <th scope='col'>#</th>
-                    <th scope='col'>Description</th>
-                    <th scope='col'>Assigned</th>
-                </tr>
-            </thead>
-            <tbody>
-            <TodoRowItem
-                rowNumber={props.todos[0].rowNumber}
-                rowDescription={props.todos[0].rowDescription}
-                rowAssigned={props.todos[0].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={props.todos[1].rowNumber}
-                rowDescription={props.todos[1].rowDescription}
-                rowAssigned={props.todos[1].rowAssigned}
-              />
-              <TodoRowItem
-                rowNumber={props.todos[2].rowNumber}
-                rowDescription={props.todos[2].rowDescription}
-                rowAssigned={props.todos[2].rowAssigned}
-              />
-            </tbody>
-        </table>
-    )
+  return (
+    <table className="table table-hover">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Description</th>
+          <th scope="col">Assigned</th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.todos.map((todo) => (
+          <TodoRowItem
+            rowNumber={todo.rowNumber}
+            rowDescription={todo.rowDescription}
+            rowAssigned={todo.rowAssigned}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
-export default TodoTable
+export default TodoTable;
